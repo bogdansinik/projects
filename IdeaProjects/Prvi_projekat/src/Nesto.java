06a1ccfe143806a8ea52f0be1f95868945020543
ljeb <bodgansinik2001@gmail.com>
2021-12-08 10:25:38 +0100

    Changed Prvi_projekat
    Counts number of a.

--- IdeaProjects/Prvi_projekat/src/Nesto.java
+++ IdeaProjects/Prvi_projekat/src/Nesto.java
@@ -1,5 +1,13 @@
 public class Nesto {
-    public static void main(String[] args) {
-        System.out.print("Jebote radi!");
+    public static void main(String [] args) {
+        String s = "Nesto lijepo je napisano ovdje i sta sad raditi sa ovim tekstom to tek treba da vidimo. Ne mogu nista ljepse da smislim tako da to je sto je.";
+        char [] c = s.toCharArray();
+        int counter = 0;
+        for(int i=0; i<c.length; i++){
+            if(c[i] == 'a'){
+                counter++;
+            }
+        }
+        System.out.println("Number of a in string = " + counter);
     }
 }
--- IdeaProjects/asfgsfdhgfdsd/nestoooo.java
+++ IdeaProjects/asfgsfdhgfdsd/nestoooo.java
@@ -1,5 +1,6 @@
 public class nestoooo {
     public static void main(String[] args) {
-        
+        String s = "Nesto lijepo je napisano ovdje i sta sad raditi sa ovim tekstom to tek treba da vidimo. Ne mogu nista ljepse da smislim tako da to je sto je.";
+        Char [] c =
     }
 }
